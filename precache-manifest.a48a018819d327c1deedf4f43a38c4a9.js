64df2cc365107d1595f98e68391a02b3f34f4b3a
WojciechSala <wojteksala190@gmail.com>
2020-04-18 20:17:58 +0200

    Updates

---
+++ precache-manifest.a48a018819d327c1deedf4f43a38c4a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5c2566a7fdd1bb9cf2b7e60908ae38",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cc3038d3f919e845e0ab",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f141faa93254d62ba5a",
+    "url": "/portfolio/static/js/2.d90ae76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d90ae76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3038d3f919e845e0ab",
+    "url": "/portfolio/static/js/main.d2d1aff7.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
