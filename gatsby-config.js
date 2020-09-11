12f30c9a4bebca58263ae6c2406fac7a1d27bc5d
WojciechSala <wojteksala190@gmail.com>
2020-09-11 18:01:08 +0200

    Initial commit from gatsby: (https://github.com/haysclark/gatsby-starter-typescript.git)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Wojciech Sala — Portfolio`,
-    author: `Wojciech Sala`,
-    description: `Check out my work and feel free to contact me.`,
-    keywords: `Portfolio, Personal website, Wojciech, Wojtek, Sala`,
+    title: `Gatsby Typescript Starter`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`
   ],
-};
+}
