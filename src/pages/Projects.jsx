8387c57978cf155930bb98e3eb20bbc6861c0667
WojciechSala <wojteksala190@gmail.com>
2020-09-20 22:02:03 +0200

    projects rwd

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,7 @@ import MasterStyle from "../assets/styles/MasterStyle"
 import SEO from "../components/shared/SEO"
 import Header from "../components/shared/Header"
 import Heading from "../components/shared/Heading"
+import Footer from "../components/shared/Footer"
 
 const Projects = () => {
   const intl = useIntl()
@@ -17,11 +18,15 @@ const Projects = () => {
       />
       <MasterStyle>
         <Header />
+        <Footer/>
         <Container>
           <Heading headingId="projects" />
           <Inner>
-            {/* map render from project list */}
+            {/* map render from project list and add animations*/}
             <ProjectLink href="projects/proj"></ProjectLink>
+            <ProjectLink></ProjectLink>
+            <ProjectLink></ProjectLink>
+            <ProjectLink></ProjectLink>
             <ProjectLink></ProjectLink>
             <ProjectLink></ProjectLink>
           </Inner>
@@ -36,21 +41,36 @@ export default Projects
 const Container = styled.div`
   width: 100%;
   padding: 0 32px;
+  padding-bottom: 100px;
 
   @media (min-width: 768px) {
     padding: 0 100px;
+    padding-bottom: 100px;
   }
 `
 const Inner = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  justify-items: center;
+  grid-gap: 50px;
+
+  @media (min-width: 768px) {
+    /* width: 768px; */
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 100px;
+  }
+  @media (min-width: 1300px) {
+    width: 1150px;
+    column-gap: 150px;
+  }
+
 `
 const ProjectLink = styled.a`
-  width: 300px;
-  height: 300px;
-  margin-bottom: 50px;
-  /* change to img of project */
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 100%;
   background-color: ${({ theme }) => theme.color.yellow};
 
   -webkit-box-shadow: 0px 3px 10px 0px rgba(0, 0, 0, 0.25);
@@ -60,5 +80,28 @@ const ProjectLink = styled.a`
   :hover {
     cursor: pointer;
     /* zoom in img */
+  }
+
+  @media (min-width: 364px) {
+    width: 300px;
+    height: 300px;
+    padding: 0;
+  }
+
+  @media (min-width: 768px) {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%;
+
+    :nth-child(even) {
+      margin-top: 100px;
+    }
+  }
+
+  @media (min-width: 1300px) {
+    width: 500px;
+    height: 500px;
+    padding: 0;
   }
 `
